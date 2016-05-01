9eac883901c9284f7f80a9c1f99d2af7dded38d0
pratik singh <pratik_cha@hotmail.com>
2016-04-30 19:30:31 -0500

    finally done

---
+++ CustomActionBar_tutorial/TestingCustomActionBar/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace TestingCustomActionBar
+{
+    [Activity(Label = "TestingCustomActionBar", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/CustomActionBarTheme")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            //ActionBar.SetDisplayShowHomeEnabled(false);
+            //ActionBar.SetDisplayShowTitleEnabled(false);
+            ActionBar.SetCustomView(Resource.Layout.MyMenu);
+            ActionBar.SetDisplayShowCustomEnabled(true);
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
